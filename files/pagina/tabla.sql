c9a96cc839812a67dbc975d8c4e432043bf108f1
EmmanuelLio05 <70165675+EmmanuelLio05@users.noreply.github.com>
2022-09-15 03:19:03 -0500

    [ADD] 1

---
+++ files/pagina/tabla.sql
@@ -0,0 +1,6 @@
+CREATE TABLE if not exists employees (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    salary INT(10) NOT NULL
+);
